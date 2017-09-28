7222c70eec05f012a9fb34d71fe44eec2572c59d
Marc Telesha <mtelesha@cscinc.org>
2017-09-28 17:38:14 -0400

    Fixed double loop when iterating over a file with files.

--- file-cleanup.rkt
+++ file-cleanup.rkt
@@ -7,8 +7,13 @@
 (provide file-cleanup)
 
 (define (file-cleanup)
-(if (file-exists? (build-path "./img"           (car (directory-list "./to_be_printed"))))
-    (delete-file  (build-path "./to_be_printed" (car (directory-list "./to_be_printed"))))
-    (copy-file    (build-path "./to_be_printed" (car (directory-list "./to_be_printed")))
-                  (build-path "./img"           (car (directory-list "./to_be_printed"))))))
-
+(cond [(file-exists? (build-path "./img" (first (directory-list "./to_be_printed"))))
+       (delete-file  (build-path "./to_be_printed" (first (directory-list "./to_be_printed"))))]
+      
+      [else
+       ;(for ([i (directory-list "./to_be_printed")]) ; This line will go through whole directory at once
+       (for ([i (list 1)]) ; This line will go through only one tile
+         (copy-file    (build-path "./to_be_printed"
+                                   (first (directory-list "./to_be_printed")))
+                       (build-path "./img"           (first (directory-list "./to_be_printed"))))
+         (delete-file  (build-path "./to_be_printed" (first (directory-list "./to_be_printed")))))]))
