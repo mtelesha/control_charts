2bf98fa36cf3db7454d72cc02de31b8c997e797d
Marc Telesha <mtelesha@cscinc.org>
2017-10-05 07:08:43 -0400

    Update file cleanup to be one loop to move or delete the file

--- file-cleanup.rkt
+++ file-cleanup.rkt
@@ -8,14 +8,14 @@
 (provide file-cleanup)
 
 (define (file-cleanup)
-(cond [(file-exists? (build-path "./img" (first (directory-list "./to_be_printed"))))
+(cond [(file-exists? (build-path "./img" (first (directory-list ".\\to_be_printed"))))
        (delete-file  (build-path "./to_be_printed" (first (directory-list "./to_be_printed"))))]
       
       [else
        ;(for ([i (directory-list "./to_be_printed")]) ; This line will go through whole directory at once
        (for ([i (list 1)]) ; This line will go through only one tile
          (copy-file    (build-path "./to_be_printed"
-                                   (first (directory-list "./to_be_printed")))
-                       (build-path "./img"           (first (directory-list "./to_be_printed"))))
-         (delete-file  (build-path "./to_be_printed" (first (directory-list "./to_be_printed")))))]))
+                                   (first (directory-list ".\\to_be_printed")))
+                       (build-path "./img"           (first (directory-list ".\\to_be_printed"))))
+         (delete-file  (build-path "./to_be_printed" (first (directory-list ".\\to_be_printed")))))]))
 
